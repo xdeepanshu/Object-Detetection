0223b4d62ad490a03414d9c65ac8b9253bc8b410
Deepanshu Rautela <deepanshu@Deepanshus-MacBook-Air.local>
2019-02-01 22:46:33 +0530

    Add logout redirect and UI changes

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_REDIRECT_URL = 'list'
-
+LOGOUT_REDIRECT_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
